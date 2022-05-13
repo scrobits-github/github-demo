35a7200ffdfa798625c79b8256f07b9b8033da47
scrobits-github <73216435+scrobits-github@users.noreply.github.com>
2022-05-13 20:08:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.get('/user', (req, res) => {
     res.send("hello World")
 })
 
+
+app.get('/data', (req, res) => {
+    res.send("hello data")
+})
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
